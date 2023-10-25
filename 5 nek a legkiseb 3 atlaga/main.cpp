2ccf5cd71f5f4b55f2dd11f2de77d13cd3718627
zsoltika <148947639+kocsiszsolt@users.noreply.github.com>
2023-10-25 10:50:23 +0300

    Add files via upload

---
+++ 5 nek a legkiseb 3 atlaga/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c,d,e,s=0;
+    cout<<"a=";
+    cin>>a;
+    cout<<"b=";
+    cin>>b;
+    cout<<"c=";
+    cin>>c;
+    cout<<"d=";
+    cin>>d;
+    cout<<"e=";
+    cin>>e;
+    if(a>b&&a>c&&a>d&&a>e){
+
+    }
+
+    return 0;
+}
